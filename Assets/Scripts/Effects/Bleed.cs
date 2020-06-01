72bdec419f34658ae28921ec525d087245171e44
peanutpowah <samuel.zelenka@hotmail.com>
2020-06-01 23:31:11 +0200

    Added effects
    Bleed,
    fear,
    stun,
    poison

---
+++ Assets/Scripts/Effects/Bleed.cs
@@ -0,0 +1,14 @@
+﻿public class Bleed : Effect
+{
+    int damage;
+    public Bleed(int damage)
+    {
+        description = $"Reduces {damage} vitality every turn.";
+        this.damage = damage;
+    }
+    public override void ApplyEffect(Character character){}
+    public override void EffectTick(Character character)
+    {
+        character.resources.Vitality -= damage; 
+    }
+}
---
+++ Assets/Scripts/Effects/Fear.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fear : Effect
+{
+    int damage;
+    public Fear(int damage)
+    {
+        description = "";
+        this.damage = damage;
+    }
+    public override void ApplyEffect(Character character) { }
+    public override void EffectTick(Character character)
+    {
+        character.resources.Vitality -= damage;
+    }
+}
