d9e36167f123e5c0beb28d06ffb71ba6c4403797
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2020-07-01 12:10:11 +0200

    Skillcheck UI

---
+++ Assets/Scripts/SkillcheckCoin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillcheckCoin : MonoBehaviour
+{
+    const float percentageToWin = 50;
+    [SerializeField] Image coinFace = null;
+    [SerializeField] Sprite winSprite = null, lossSprite = null, undecidedSprite = null;
+    const float flipTime = 0.5f;
+    const float maxRotation = 50f;
+
+    public static bool FlipCoin() => Random.Range(0, 100) >= percentageToWin;
+
+    public void TurnCoinToUndecided()
+    {
+        coinFace.sprite = undecidedSprite;
+    }
+
+    public IEnumerator VisualizeCoinFlip(bool win)
+    {
+        float flipTimer = 0;
+
+        //Rotate
+        while (flipTimer < flipTime)
+        {
+            float rotation = maxRotation * (flipTime - flipTimer) / flipTime;
+            coinFace.sprite = Utility.ReturnRandom(winSprite, lossSprite);
+            this.transform.Rotate(new Vector3(0, rotation, 0));
+            yield return null;
+            flipTimer += Time.deltaTime;
+        }
+
+        this.transform.rotation = Quaternion.identity;
+        coinFace.sprite = win ? winSprite : lossSprite;
+    }
+}
---
+++ Assets/Scripts/SkillcheckSystem.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillcheckSystem : MonoBehaviour
+{
+    const int numberOfCoins = 4;
+
+    public enum CombatOutcome { Miss, Grace, NormalHit, Critical }
+
+    public delegate void CombatOutcomeHandler(CombatOutcome combatOutcome);
+    public static CombatOutcomeHandler OnCombatOutcomeDecided;
+
+    [Header("References")]
+    //[SerializeField] GameObject[] engagerCoins = null;
+
+    [SerializeField] SkillcheckCoin[] attackerCoins = new SkillcheckCoin[numberOfCoins], defenderCoins = new SkillcheckCoin[numberOfCoins];
+    [SerializeField] Text attackerStartValueText = null, attackerEndValueText = null;
+    [SerializeField] Text defenderStartValueText = null, defenderEndValueText = null;
+    [SerializeField] Text outcomeText = null;
+
+    int attackerEndValue = 0, defenderEndValue = 0;
+
+    public void DebugStartContested()
+    {
+        StartContestedSkillcheck(Random.Range(0, 10), Random.Range(0, 10));
+    }
+
+    public void StartContestedSkillcheck(int attackerStartValue, int defenderStartValue)
+    {
+        outcomeText.text = "";
+
+        this.attackerStartValueText.text = attackerStartValue.ToString();
+        this.defenderStartValueText.text = defenderStartValue.ToString();
+
+        attackerEndValue = attackerStartValue;
+        defenderEndValue = defenderStartValue;
+
+        attackerEndValueText.text = attackerEndValue.ToString();
+        defenderEndValueText.text = defenderEndValue.ToString();
+
+        StartCoroutine(PerformContestedSkillcheck());
+    }
+
+    IEnumerator PerformContestedSkillcheck()
+    {
+        foreach (var item in attackerCoins)
+        {
+            item.TurnCoinToUndecided();
+        }
+
+        foreach (var item in defenderCoins)
+        {
+            item.TurnCoinToUndecided();
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        int attackerSuccesses = 0;
+
+        for (int i = 0; i < numberOfCoins; i++)
+        {
+            bool attackerSuccess = SkillcheckCoin.FlipCoin();
+            if (attackerSuccess)
+            {
+                attackerSuccesses++;
+            }
+
+            bool defenderSuccess = SkillcheckCoin.FlipCoin();
+
+            attackerEndValue = attackerSuccess ? attackerEndValue + 1 : attackerEndValue - 1;
+            defenderEndValue = defenderSuccess ? defenderEndValue + 1 : defenderEndValue - 1;
+
+            StartCoroutine(attackerCoins[i].VisualizeCoinFlip(attackerSuccess));
+            yield return defenderCoins[i].VisualizeCoinFlip(defenderSuccess);
+
+            //Write text
+            attackerEndValueText.text = attackerEndValue.ToString();
+            defenderEndValueText.text = defenderEndValue.ToString();
+        }
+        DecideContestedOutcome(attackerSuccesses);
+    }
+
+    private void DecideContestedOutcome(int attackerSuccesses)
+    {
+        CombatOutcome combatOutcome = CombatOutcome.Miss;
+        if (attackerSuccesses == numberOfCoins) //All successes = crit always
+        {
+            combatOutcome = CombatOutcome.Critical;
+        }
+        else if (attackerSuccesses == 0) //All misses = miss always
+        {
+            combatOutcome = CombatOutcome.Miss;
+        }
+        else
+        {
+            if (attackerEndValue >= defenderEndValue)
+            {
+                combatOutcome = attackerSuccesses == 1 ? CombatOutcome.Grace : CombatOutcome.NormalHit;
+            }
+        }
+        outcomeText.text = OutcomeString(combatOutcome);
+
+        OnCombatOutcomeDecided?.Invoke(combatOutcome);
+    }
+
+    private string OutcomeString(CombatOutcome combatOutcome)
+    {
+        switch (combatOutcome)
+        {
+            case CombatOutcome.Miss:
+                return "Miss";
+            case CombatOutcome.Grace:
+                return "Grace";
+            case CombatOutcome.NormalHit:
+                return "Hit";
+            case CombatOutcome.Critical:
+                return "Critical Hit";
+            default:
+                return "";
+        }
+    }
+}
