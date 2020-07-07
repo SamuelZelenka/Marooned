55c53534df0d4cebb1fd1d7637651d0c455c8ea6
peanutpowah <samuel.zelenka@hotmail.com>
2020-07-07 05:37:27 +0200

    Effect icons over character in game

--- Assets/Scripts/UI/CharacterOverHeadUI.cs
+++ Assets/Scripts/UI/CharacterOverHeadUI.cs
@@ -8,6 +8,14 @@ public class CharacterOverHeadUI : MonoBehaviour
     [SerializeField] Bar vitality = null;
     [SerializeField] Bar loyalty = null;
 
+    [SerializeField] MouseHoverImage prefab = null;
+    [SerializeField] Transform effectParent = null;
+
+    List<MouseHoverImage> activeEffects = new List<MouseHoverImage>();
+
+
+
+
     private void OnEnable()
     {
         CharacterData.OnResourceChanged += UpdateUI;
@@ -23,5 +31,34 @@ public class CharacterOverHeadUI : MonoBehaviour
         vitality.SetMaxValue(character.characterData.Vitality.maxValue);
         loyalty.SetCurrentValue(character.characterData.Loyalty.CurrentValue);
         loyalty.SetMaxValue(character.characterData.Loyalty.maxValue);
+
+        if (SyncEffectLists())
+        {
+            for (int i = 0; i < character.characterData.activeEffects.Count; i++)
+            {
+                string effectDescription = character.characterData.activeEffects[i].Description;
+                Sprite effectSprite = character.characterData.activeEffects[i].EffectSprite;
+                activeEffects[i].UpdateUI(effectDescription, effectSprite);
+            }
+        }
+
+        bool SyncEffectLists()
+        {
+            if (activeEffects.Count < character.characterData.activeEffects.Count)
+            {
+                while (activeEffects.Count < character.characterData.activeEffects.Count)
+                {
+                    activeEffects.Add(Instantiate(prefab, effectParent));
+                }
+            }
+            if (activeEffects.Count > character.characterData.activeEffects.Count)
+            {
+                while (activeEffects.Count > character.characterData.activeEffects.Count)
+                {
+                    activeEffects.RemoveAt(0);
+                }
+            }
+            return activeEffects.Count == character.characterData.activeEffects.Count;
+        }
     }
 }
