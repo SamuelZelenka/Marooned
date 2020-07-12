d6e5e1e59604f9eb29bbbcf1a05c2fba27c69437
peanutpowah <samuel.zelenka@hotmail.com>
2020-07-13 00:29:52 +0200

    Hover over abilities now show information

--- Assets/Scripts/UI/AbilityUI.cs
+++ Assets/Scripts/UI/AbilityUI.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class AbilityUI : MonoBehaviour
 {
     bool isReady;
-    [SerializeField] Image abilityImage;
-    [SerializeField] Image disabledImage;
-    [SerializeField] Text abilityCost;
-    [SerializeField] Button abilityButton;
-    
+    [SerializeField] Image abilityImage = null;
+    [SerializeField] Image disabledImage = null;
+    [SerializeField] Text abilityCost = null;
+    [SerializeField] Button abilityButton = null;
+    [SerializeField] MouseHoverImage enabledHoverImage = null;
+
+
     public void UpdateUI(Ability ability)
     {
 
@@ -18,11 +20,23 @@ public class AbilityUI : MonoBehaviour
         {
             if (ability.cost - 1 < HexGridController.ActiveCharacter.characterData.Energy.CurrentValue)
             {
+                enabledHoverImage.UpdateUI(
+                    $"{ability.abilityName}\n" +
+                    $"{ability.abilityDescription} \n\n" +
+                    $"<color=#00ff00> Ready </color> \n" +
+                    $"{ability.cost} Energy",
+                    ability.AbilitySprite);
                 abilityImage.sprite = ability.AbilitySprite;
                 Enabled(true);
             }
             else
             {
+                enabledHoverImage.UpdateUI(
+                    $"{ability.abilityName}\n" +
+                    $"{ability.abilityDescription} \n\n" +
+                    $"<color=red> Not enough energy </color> \n" +
+                    $"{ability.cost} Energy",
+                    ability.AbilitySprite);
                 abilityCost.text = ability.cost.ToString();
                 Enabled(false);
             }
