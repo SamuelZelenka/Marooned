54b85f13d6e93323b2cf1a2d0b1a45238c464e42
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2020-07-14 15:53:39 +0200

    Prefabs and spawnable objects

--- Assets/Scripts/Units/Character.cs
+++ Assets/Scripts/Units/Character.cs
@@ -167,7 +167,7 @@ public class Character : HexUnit
 
     public override bool CanEnter(HexCell cell)
     {
-        if (cell.Unit)
+        if (!cell.IsFree)
         {
             return false;
         }
